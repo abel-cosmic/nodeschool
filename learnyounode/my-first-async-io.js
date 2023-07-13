3b7f715a1666095594c36c743ecc690b60a615d4
Abel Shibabaw <abelshibabaw@heuc.edu.et>
2023-07-13 19:29:42 +0300

    finished my-first-async-io.js

---
+++ learnyounode/my-first-async-io.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+
+const filePath = process.argv[2];
+
+fs.readFile(filePath, 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+    /*
+    return console.log(err);
+    */
+  }
+
+  const newlineCount = data.split('\n').length - 1;
+  console.log(newlineCount);
+});
